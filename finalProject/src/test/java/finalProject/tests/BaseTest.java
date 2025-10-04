f15ed12fd62211148db161936601a8cdb0d774b7
[username] <[email]>
2025-10-04 18:19:42 +0200

    change before method

--- finalProject/src/test/java/finalProject/tests/BaseTest.java
+++ finalProject/src/test/java/finalProject/tests/BaseTest.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.Selenide;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import static com.codeborne.selenide.Selenide.$;
 import static finalProject.TestRailController.publishResults;
 
 public class BaseTest {
@@ -13,6 +15,9 @@ public class BaseTest {
     @BeforeMethod
     public void openSite(){
         Selenide.open("https://www.bazar-ua.shop/");
+        $("div.button-setting").click();
+        $("span.dropdown-toggle").click();
+        $("[href='Ua']").click();
     }
 
     @AfterMethod
